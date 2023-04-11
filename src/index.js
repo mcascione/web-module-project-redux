79147fe1d0046fd11a57333e925486d9ccadbe70
Miranda Cascione <miranda.cascione@gmail.com>
2023-04-11 12:04:37 -0700

    connected movie reducer to components MovieList, Movie, and MovieHeader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
+import movieReducer from './reducers';
 
 import reducer from './reducers';
 
@@ -9,7 +10,7 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
-const store = createStore(reducer);
+const store = createStore(movieReducer);
 
 ReactDOM.render(
   <Provider store={store}>
